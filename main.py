1ef826cf86a07004707d6ee8087d61b9459398d3
Patrick Stover <stoverp@colorado.edu>
2023-02-10 12:50:59 -0700

    simple pawn promotions, move ordering 1st go, highlight last move dest

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ from pygame.display import set_mode
 from pygame.image import load
 from pygame.rect import Rect
 from pygame.sprite import Sprite
+from sortedcontainers import SortedList
+
 
 SEARCH_DEPTH = 3
 
@@ -18,7 +20,8 @@ DARK_SQUARES = 1
 BACKGROUND_COLORS = [(242, 210, 133), (115, 99, 61)]
 SELECTED_SQUARE_COLOR = (252, 186, 3)
 LEGAL_MOVE_COLORS = [(235, 127, 132), (115, 61, 63)]
-LAST_MOVE_COLORS = [(127, 235, 226), (61, 113, 115)]
+LAST_MOVE_START_COLORS = [(127, 235, 226), (61, 113, 115)]
+LAST_MOVE_DEST_COLORS = [(127, 235, 165), (61, 115, 78)]
 
 # todo: limit window resizing to preserve aspect ratio
 DISPLAY_WIDTH = 1120
@@ -129,7 +132,7 @@ class Piece(Sprite):
     self.rank = rank
     self.file = file
     self.n_times_moved = 0
-    self.surface = load(f"images/Chess_{type.value}{player_color.image_abbr}t60.png")
+    self.surface = self.load_image()
 
   def __str__(self):
     return f"Piece(player_color={self.player_color}, type={self.type}, rank={self.rank}, file={self.file})"
@@ -137,21 +140,33 @@ class Piece(Sprite):
   def __repr__(self):
     return str(self)
 
+  def load_image(self):
+    return load(f"images/Chess_{self.type.value}{self.player_color.image_abbr}t60.png")
+
   def fen(self):
     abbr = self.type.value
     return abbr.upper() if self.player_color is PlayerColor.WHITE else abbr
 
+  def update_type(self, new_type):
+    pieces = Globals.players[self.player_color].pieces
+    pieces[self.type].remove(self)
+    self.type = new_type
+    self.surface = self.load_image()
+    pieces[new_type].add(self)
+
 
 class Move:
-  def __init__(self, piece, rank, file):
+  def __init__(self, piece, rank, file, promote_type=None):
     self.piece = piece
     self.rank = rank
     self.file = file
+    self.promote_type = promote_type
     # todo: this assumes that a Move is created only when the board is in the appropriate state
     self.captured_piece = Globals.board[rank][file]
     self.old_rank = piece.rank
     self.old_file = piece.file
     self.castling_rook_move = None
+    self.guess_score()
 
   def __str__(self):
     return f"Move(piece={self.piece}, rank={self.rank}, file={self.file}, captured_piece={self.captured_piece}, old_rank={self.old_rank}, old_file={self.old_file})"
@@ -172,6 +187,8 @@ class Move:
       Globals.players[self.captured_piece.player_color].pieces[self.captured_piece.type].remove(self.captured_piece)
     self.piece.rank = self.rank
     self.piece.file = self.file
+    if self.promote_type:
+      self.piece.update_type(self.promote_type)
     Globals.board[self.old_rank][self.old_file] = None
     Globals.board[self.rank][self.file] = self.piece
     self.piece.n_times_moved += 1
@@ -191,12 +208,21 @@ class Move:
       Globals.players[self.captured_piece.player_color].pieces[self.captured_piece.type].add(self.captured_piece)
     self.piece.rank = self.old_rank
     self.piece.file = self.old_file
+    if self.promote_type:
+      self.piece.update_type(PieceType.PAWN)
     Globals.board[self.rank][self.file] = self.captured_piece
     Globals.board[self.old_rank][self.old_file] = self.piece
     self.piece.n_times_moved -= 1
     # handle castling special case
     if self.castling_rook_move:
       self.castling_rook_move.unapply()
+
+  def guess_score(self):
+    self.score_guess = 0
+    if self.captured_piece:
+      self.score_guess = 10 * self.captured_piece.type.score - self.piece.type.score
+    if self.promote_type:
+      self.score_guess += self.promote_type.score
 
 
 def display_to_screen(display_pos):
@@ -276,6 +302,7 @@ class Globals:
   active_player_color = PlayerColor.WHITE
   active_player_legal_moves = []
   move_history = []
+  n_moves_searched = 0
 
   @classmethod
   def active_player(cls):
@@ -283,11 +310,14 @@ class Globals:
 
 
 def draw_squares():
+  last_move = Globals.move_history[-1] if Globals.move_history else None
   for rank in range(8):
     for file in range(8):
       square_type = (rank + file) % 2
-      if Globals.move_history and (rank, file) == (Globals.move_history[-1].old_rank, Globals.move_history[-1].old_file):
-        color = LAST_MOVE_COLORS[square_type]
+      if last_move and (rank, file) == (last_move.old_rank, last_move.old_file):
+        color = LAST_MOVE_START_COLORS[square_type]
+      elif last_move and (rank, file) == (last_move.rank, last_move.file):
+        color = LAST_MOVE_DEST_COLORS[square_type]
       elif Globals.selected:
         if (rank, file) == (Globals.selected.piece.rank, Globals.selected.piece.file):
           color = SELECTED_SQUARE_COLOR
@@ -323,6 +353,18 @@ def get_screen_pos(rank, file):
   return file * SQUARE_WIDTH, (7 - rank) * SQUARE_WIDTH
 
 
+def is_promoting_pawn(piece, new_rank):
+  return piece.type is PieceType.PAWN and new_rank == piece.player_color.opponent.back_rank
+
+
+def include_promotion_moves(pawn, rank, file, moves):
+  if is_promoting_pawn(pawn, rank):
+    for promote_type in [PieceType.QUEEN, PieceType.KNIGHT]:
+      moves.add(Move(pawn, rank, file, promote_type=promote_type))
+  else:
+    moves.add(Move(pawn, rank, file))
+
+
 def generate_pawn_moves(piece):
   moves = set()
   direction = 1 if piece.player_color is PlayerColor.WHITE else -1
@@ -335,14 +377,13 @@ def generate_pawn_moves(piece):
       rank_candidates.append(piece.rank + (2 * direction))
   for new_rank in rank_candidates:
     if MoveType.get_type(piece.player_color, new_rank, piece.file) is MoveType.OPEN_SQUARE:
-      moves.add(Move(piece, new_rank, piece.file))
+      include_promotion_moves(piece, new_rank, piece.file, moves)
   # capture moves
   for rank_offset, file_offset in [(direction, 1), (direction, -1)]:
     new_rank, new_file = piece.rank + rank_offset, piece.file + file_offset
     if MoveType.get_type(piece.player_color, new_rank, new_file) is MoveType.CAPTURE:
-      moves.add(Move(piece, new_rank, new_file))
+      include_promotion_moves(piece, new_rank, new_file, moves)
   # todo: en passant
-  # todo: promotion
   return moves
 
 
@@ -444,12 +485,16 @@ def generate_legal_moves(piece, filter_checks=True):
 
 
 def generate_all_legal_moves(player, filter_checks=True):
-  all_legal_moves = []
+  # keep move list in sorted order by score guess
+  all_legal_moves = SortedList(key=lambda t: t[0])
+  # all_legal_moves = []
   for pieces in player.pieces.values():
     for piece in pieces:
       for move in generate_legal_moves(piece, filter_checks):
-        all_legal_moves.append(move)
-  return all_legal_moves
+        all_legal_moves.add((move.score_guess, move))
+        # all_legal_moves.append(move)
+  return [move for score_guess, move in all_legal_moves]
+  # return all_legal_moves
 
 
 def undo_last_move():
@@ -491,6 +536,7 @@ def search_moves(active_player_color, depth, alpha, beta):
   for move in moves:
     # if move.rank == 5 and move.file == 0 and move.captured_piece:
     #   print(f"evaluating {move} ...")
+    Globals.n_moves_searched += 1
     move.apply()
     _, score = search_moves(active_player_color.opponent, depth - 1, -beta, -alpha)
     # negate score to reflect opponent's perspective
@@ -508,10 +554,11 @@ def search_moves(active_player_color, depth, alpha, beta):
 
 
 def best_move(active_player_color):
+  Globals.n_moves_search = 0
   print(f"\ncalculating {active_player_color} move ...")
   start_time = time.time()
   move, score = search_moves(active_player_color, SEARCH_DEPTH, -math.inf, math.inf)
-  print(f"evaluated score {score} in {time.time() - start_time} seconds for {move}")
+  print(f"evaluated score {score} by searching {Globals.n_moves_searched} moves in {time.time() - start_time} seconds for {move}")
   if move:
     return move
   else:
@@ -531,6 +578,8 @@ def endgame():
   while waiting:
     for event in pg.event.get():
       if event.type == pg.KEYDOWN:
+        waiting = False
+      if event.type == pg.QUIT:
         waiting = False
 
 
@@ -565,6 +614,11 @@ def generate_fen():
   return f"{'/'.join(piece_placement_ranks)} {Globals.active_player_color.abbr} {generate_castling_ability_fen()} - - -"
 
 
+def get_user_promote_type():
+  # todo: prompt for piece type
+  return PieceType.QUEEN
+
+
 def handle_event(event, vs_human):
   if event.type == pg.QUIT:
     return False
@@ -577,7 +631,10 @@ def handle_event(event, vs_human):
   elif event.type == pg.MOUSEBUTTONUP:
     if Globals.selected and event.pos:
       rank, file = get_square(event.pos)
-      move = Move(Globals.selected.piece, rank, file)
+      if is_promoting_pawn(Globals.selected.piece, rank):
+        move = Move(Globals.selected.piece, rank, file, promote_type=get_user_promote_type())
+      else:
+        move = Move(Globals.selected.piece, rank, file)
       if move in Globals.selected.legal_moves:
         make_move(move)
         print(f"\nCURRENT BOARD FEN: {generate_fen()}")
